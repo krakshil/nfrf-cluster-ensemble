b53ec2e1f10e248920abae920a4540dc4f839650
krakshil <krakshil07@gmail.com>
2024-03-05 12:31:58 -0500

    embedding_selection

---
+++ embedding_selection.py
@@ -0,0 +1,195 @@
+## import packages
+import os
+import string
+import glob
+import json
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from umap import UMAP
+from hdbscan import HDBSCAN
+from bertopic import BERTopic
+from bertopic.dimensionality import BaseDimensionalityReduction
+from sentence_transformers import SentenceTransformer
+import tensorflow as tf
+import tensorflow_hub as hub
+from sklearn.cluster import SpectralClustering
+from sklearn.mixture import GaussianMixture
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import ParameterGrid
+from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score, adjusted_rand_score, adjusted_mutual_info_score
+
+
+## model definition
+class topicModel():
+    def __init__(self, train_docs:list, test_docs:list, embedding_selection_config:dict, save_dir=""):
+        """
+        docs: list (list of documents for topic modelling)
+
+        embedding_selection_config: dict (contain metadata of the embedding models, umap hyper-paraters and hdbscan hyper-parameters)
+            {"embedding_models": {"model_name": ["type", "url_or_path"]},
+             "umap_params_dict": {"n_neighbors":[], "min_dist":[], "n_components":[], "metric":[], "random_state":[42]},
+             "hdbscan_params_dict": {"min_cluster_size":[15,20,25], "min_samples":[5,10,15], "cluster_selection_epsilon":[0.4, 0.5, 0.7]}
+            }
+        """
+
+        self.train_docs = train_docs
+        self.test_docs = test_docs
+        
+        self.embedding_selection_config = embedding_selection_config
+        self.embedding_selection_umap_params_list = list(ParameterGrid(self.embedding_selection_config["umap_params_dict"]))
+        self.embedding_selection_hdbscan_params_list = list(ParameterGrid(self.embedding_selection_config["hdbscan_params_dict"]))
+        self.embedding_selection_dict = {key: {} for key in self.embedding_selection_config["embedding_models"].keys()}
+
+        # self.embedding_config = embedding_config
+        
+        # self.clustering_config = clustering_config
+        # # self.clustering_dict = {key: {} for key in clustering_config.keys()}
+        # self.vectorizer_config = vectorizer_config
+        # self.ctfidf_config = ctfidf_config
+        # self.representation_config = representation_config
+        # self.evaluation_dict = {}
+        self.selection_evaluation_dict = {}
+        self.save_directory = os.path.join(save_dir, "results")
+    
+    
+    ## compute embeddings for each model (reduce calculation of embeddings for each run)
+    def compute_selection_embeddings(self, create_save_dir=True): 
+        for model_name in self.embedding_selection_dict.keys():
+            if self.embedding_selection_config["embedding_models"][model_name][0] == "tensorflow":
+                self.embedding_selection_dict[model_name]["model"] = hub.load(self.embedding_selection_config["embedding_models"][model_name][1])
+                self.embedding_selection_dict[model_name]["train_embeddings"] = self.embedding_selection_dict[model_name]["model"](self.train_docs).numpy()
+                self.embedding_selection_dict[model_name]["test_embeddings"] = self.embedding_selection_dict[model_name]["model"](self.test_docs).numpy()
+        
+
+            elif self.embedding_selection_config["embedding_models"][model_name][0] == "sentence_transformer":
+                self.embedding_selection_dict[model_name]["model"] = SentenceTransformer(self.embedding_selection_config["embedding_models"][model_name][1])
+                self.embedding_selection_dict[model_name]["train_embeddings"] = self.embedding_selection_dict[model_name]["model"].encode(self.train_docs)
+                self.embedding_selection_dict[model_name]["test_embeddings"] = self.embedding_selection_dict[model_name]["model"].encode(self.test_docs)
+
+        self.save_selection_embeddings(umap=False, create_save_dir=create_save_dir)
+    
+
+    ## save selection embeddings
+    def save_selection_embeddings(self, create_save_dir:bool=False):
+
+        if create_save_dir:
+            os.makedirs(os.path.join(self.save_directory,"embedding_selection"), exist_ok=True)
+        
+        # with open(os.path.join(self.save_directory, "embedding_selection", "docs.json"), "w") as f:
+        #     f.write(json.dumps({"train_docs":self.train_docs, "test_docs":self.test_docs}))
+
+        for model_name in self.embedding_selection_dict.keys():
+            os.makedirs(os.path.join(self.save_directory, "embedding_selection", model_name), exist_ok=True)
+            os.makedirs(os.path.join(self.save_directory, "embedding_selection", model_name, "selection_results"), exist_ok=True)
+            train_embeddings = self.embedding_selection_dict[model_name]["train_embeddings"]
+            test_embeddings = self.embedding_selection_dict[model_name]["test_embeddings"]
+
+            with open(os.path.join(self.save_directory, "embedding_selection", model_name, "embeddings.npz"), "wb") as f:
+                np.savez(f, train_embeddings=train_embeddings, test_embeddings=test_embeddings)
+    
+
+    ## load selection embeddings
+    def load_selection_embeddings(self):
+
+        model_names = self.embedding_selection_dict["embedding_models"].keys()
+            
+        for model_name in model_names:
+            embeddings = np.load(os.path.join(self.save_directory, "embedding_selection", model_name, "embeddings.npz"))
+            self.embedding_selection_dict[model_name]["model"] = None
+            self.embedding_selection_dict[model_name]["train_embeddings"] = embeddings["train_embeddings"]
+            self.embedding_selection_dict[model_name]["test_embeddings"] = embeddings["test_embeddings"]
+    
+    
+    ## run selection model
+    def run_selection(self, load_embeddings=False, create_save_dir=True, verbose=False):
+        
+        if load_embeddings:
+            print("[INFO] Loading embeddings and features from the documents using embedding models...")
+            self.load_embeddings()
+            print("[INFO] Embeddings loaded. Starting iterations of topic modelling...\n")
+        else:
+            print("[INFO] Computing embeddings from the documents using embedding models...")
+            self.compute_selection_embeddings(create_save_dir=create_save_dir)
+            print("[INFO] Embeddings calculated and saved. Starting iterations of topic modelling...\n")
+
+        
+        with open(os.path.join(self.save_directory, "embedding_selection", "umap_params_list.json"), "w") as f:
+            json.dump(self.embedding_selection_umap_params_list, f)
+        
+        with open(os.path.join(self.save_directory, "embedding_selection", "hdbscan_params_list.json"), "w") as f:
+            json.dump(self.embedding_selection_hdbscan_params_list, f)
+
+        empty_dimensionality_model = BaseDimensionalityReduction()
+
+        vectorizer_model = None
+        ctfidf_model = None
+        representation_model = None
+
+        num_total_variants = len(list(self.embedding_selection_dict.keys()))*len(self.embedding_selection_umap_params_list)*len(self.embedding_selection_hdbscan_params_list)
+        try:
+            with tqdm(total=num_total_variants, desc="Progress") as pbar:
+                
+                for model_idx, model_name in enumerate(self.embedding_selection_dict.keys()):
+
+                    train_embeddings = self.embedding_selection_dict[model_name]["train_embedding"]
+                    test_embeddings = self.embedding_selection_dict[model_name]["test_embedding"]
+
+                    for umap_idx, umap_param in enumerate(self.embedding_selection_umap_params_list):
+                        
+                        umap_model = UMAP(**umap_param)
+                        train_features = umap_model.fit_transform(train_embeddings)
+                        test_features = umap_model.transform(test_embeddings)
+
+                        selection_evaluation_list = []
+                        
+                        for hdb_idx, hdbscan_param in enumerate(self.embedding_selection_hdbscan_params_list):
+                            
+                            cluster_model = HDBSCAN(**hdbscan_param)
+                            topic_model, (train_labels, train_probs) = self.fit(self.train_docs, train_features, empty_dimensionality_model, cluster_model, vectorizer_model, ctfidf_model, representation_model, verbose=verbose)
+                            (test_labels, test_probs) = topic_model.transform(self.test_docs, test_features)
+                            train_s_score, train_d_score, train_c_score = self.calculate_internal_scores(features=train_features, labels=train_labels)
+                            test_s_score, test_d_score, test_c_score = self.calculate_internal_scores(features=test_features, labels=test_labels)
+
+                            selection_evaluation_list.append([[train_s_score, train_d_score, train_c_score],[test_s_score, test_d_score, test_c_score]])
+                            pbar.update(1)
+                            ################ incomplete: save each results in CSV and folder start here #######################
+                            
+                        # save_path = os.path.join(self.save_directory, model_name, cluster_model_name, save_name)
+                    
+                        result_file_name = "___".join(str(key) + "_" + str(value) for (key, value) in umap_param.items()) + ".json"
+                        with open(os.path.exists(self.save_directory, "embedding_selection", model_name, "selection_results", result_file_name), "w") as f:
+                            json.dump(selection_evaluation_list, f)
+            
+                    print("[INFO] " + model_name + ": Completed.\n")
+        
+                print("[INFO] All iterations complete. All the data was stored.")
+            
+        except:
+            print("Model Index: " + str(model_idx) + ", UMAP Index:" + str(umap_idx) + ", HDBSCAN Index:" + str(hdb_idx))
+    
+
+    ## train model
+    def fit(self, docs, embeddings, umap_model, clustering_model, vectorizer_model=None, ctfidf_model=None, representation_model=None, verbose=False):
+        
+        topic_model = BERTopic(umap_model=umap_model, hdbscan_model=clustering_model, vectorizer_model=vectorizer_model, ctfidf_model=ctfidf_model, representation_model=representation_model, calculate_probabilities=True, low_memory=True, verbose=verbose)
+        
+        labels, probs = topic_model.fit_transform(docs, embeddings=embeddings)
+        # freqs = topic_model.get_topic_info()
+        # tops = topic_model.get_topics()
+        # representation_docs = topic_model.get_representative_docs()
+        # question_topic_info = topic_model.get_document_info(docs)
+        # topic_embeddings = topic_model.topic_embeddings_
+        # hierarchical_topics = topic_model.hierarchical_topics(docs)
+        # topic_tree = topic_model.get_topic_tree(hierarchical_topics)
+
+        return topic_model, (labels, probs)
+
+    
+    ## calculate internal metrics for a model
+    def calculate_internal_scores(self, features, labels):
+        try:
+            s_score, d_score, c_score = silhouette_score(X=features, labels=labels), davies_bouldin_score(X=features, labels=labels), calinski_harabasz_score(X=features, labels=labels)
+        except:
+            s_score, d_score, c_score = -1, float('inf'), 0
+        return (s_score, d_score, c_score)
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 from hdbscan import HDBSCAN
 from sklearn.cluster import KMeans, Birch
-from model import topicModel, CustomSpectralClustering, CustomGaussianMixture
+# from model import topicModel, CustomSpectralClustering, CustomGaussianMixture
+from embedding_selection import topicModel
+
 
 data_dir = os.path.join("data", "preprocessed")
 train_file = "questions_list_unique_train.xlsx"
@@ -14,37 +16,47 @@ test_docs = pd.read_excel(os.path.join(data_dir, test_file))["Follow - up Questi
 
 save_dir = os.path.join("data", "results")
 
-## Add UMAP configuration for each embedding model
-embedding_config = {
-    "ST-MiniLM": {"type":"sentence_transformer", "url":"all-MiniLM-L6-v2", "umap_params":{"n_neighbors":25, "min_dist":0.25, "n_components":10, "metric":"cosine", "low_memory":False, "random_state":42}},
-    "ST-mnpnet": {"type":"sentence_transformer", "url":"all-mpnet-base-v2", "umap_params":{"n_neighbors":25, "min_dist":0.25, "n_components":10, "metric":"cosine", "low_memory":False, "random_state":42}},
-    "USE": {"type":"tensorflow", "url":"https://tfhub.dev/google/universal-sentence-encoder/4", "umap_params":{"n_neighbors":25, "min_dist":0.25, "n_components":10, "metric":"cosine", "low_memory":False, "random_state":42}}
+## Add hyper-parameter combinations for embedding selection
+embedding_selection_config = {
+    "embedding_models": {"ST-MiniLM": ["sentence_transformer", "all-MiniLM-L6-v2"]},
+    "umap_params_dict": {"n_neighbors":[25], "min_dist":[0.25], "n_components":[10], "metric":["cosine"], "low_memory":[False], "random_state":[42]},
+    "hdbscan_params_dict": {"min_cluster_size":[30], "min_samples":[20], "cluster_selection_epsilon":[0.7], "metric":["euclidean"], "prediction_data":[True]}
 }
 
+
+# ## Add UMAP configuration for each embedding model
+# embedding_config = {
+#     "ST-MiniLM": {"type":"sentence_transformer", "url":"all-MiniLM-L6-v2", "umap_params":{"n_neighbors":25, "min_dist":0.25, "n_components":10, "metric":"cosine", "low_memory":False, "random_state":42}},
+#     "ST-mnpnet": {"type":"sentence_transformer", "url":"all-mpnet-base-v2", "umap_params":{"n_neighbors":25, "min_dist":0.25, "n_components":10, "metric":"cosine", "low_memory":False, "random_state":42}},
+#     "USE": {"type":"tensorflow", "url":"https://tfhub.dev/google/universal-sentence-encoder/4", "umap_params":{"n_neighbors":25, "min_dist":0.25, "n_components":10, "metric":"cosine", "low_memory":False, "random_state":42}}
+# }
+
 ## Add hyper-parameter combinations - incomplete (test each algorithms)
-clustering_config = {
-    "hdbscan": {"constructor":HDBSCAN, "type":"density-based", "params_dict":{"min_cluster_size":[5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75], "min_samples":[1, 5, 10, 15, 20, 25, 30, 35], "cluster_selection_epsilon":[0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.2, 1.3, 2], "metric":["euclidean","manhattan"], "prediction_data":[True]}},
+# clustering_config = {
+#     "hdbscan": {"constructor":HDBSCAN, "type":"density-based", "params_dict":{"min_cluster_size":[5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75], "min_samples":[1, 5, 10, 15, 20, 25, 30, 35], "cluster_selection_epsilon":[0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.2, 1.3, 2], "metric":["euclidean","manhattan"], "prediction_data":[True]}},
     
-    "birch": {"constructor":Birch, "type":"birch", "params_dict":{"threshold":[0.25, 0.37, 0.5, 0.62, 0.75, 0.87, 1, 1.25, 1.5, 2], "branching_factor":[10, 20, 30, 40, 50, 60, 70, 80, 90, 100], "n_clusters":[None, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]}},
+#     "birch": {"constructor":Birch, "type":"birch", "params_dict":{"threshold":[0.25, 0.37, 0.5, 0.62, 0.75, 0.87, 1, 1.25, 1.5, 2], "branching_factor":[10, 20, 30, 40, 50, 60, 70, 80, 90, 100], "n_clusters":[None, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]}},
     
-    "kmeans": {"constructor":KMeans, "type":"k-means", "params_dict":{"n_clusters":[3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], "max_iter":[300, 400, 500, 600, 700], "n_init":[1, 10, 20, 30, 40, 50], "init":["k-means++", "random"], "algorithm":["lloyd","elkan"], "random_state":[42]}},
+#     "kmeans": {"constructor":KMeans, "type":"k-means", "params_dict":{"n_clusters":[3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], "max_iter":[300, 400, 500, 600, 700], "n_init":[1, 10, 20, 30, 40, 50], "init":["k-means++", "random"], "algorithm":["lloyd","elkan"], "random_state":[42]}},
     
-    "spectral": {"constructor":CustomSpectralClustering, "type":"spectral", "params_dict":[{"n_clusters":[5, 6, 7, 8, 9, 10], "n_components":[3,5,7], "affinity":["nearest_neighbors"], "n_neighbors":[5, 10, 15, 20, 25], "assign_labels":["kmeans","discretize", "cluster_qr"], "n_jobs":[-1], "random_state":[42]},
-                                                                                           {"n_clusters":[5, 6, 7, 8, 9, 10], "n_components":[3,5,7], "affinity":["rbf", "cosine", "laplacian", "sigmoid"], "gamma":[1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1], "assign_labels":["kmeans","discretize", "cluster_qr"], "random_state":[42]},
-                                                                                           {"n_clusters":[5, 6, 7, 8, 9, 10], "n_components":[3,5,7], "affinity":["polynomial"], "degree":[3,4,5,6,7], "assign_labels":["kmeans","discretize", "cluster_qr"], "random_state":[42]},
-                                                                                           ]},
+#     "spectral": {"constructor":CustomSpectralClustering, "type":"spectral", "params_dict":[{"n_clusters":[5, 6, 7, 8, 9, 10], "n_components":[3,5,7], "affinity":["nearest_neighbors"], "n_neighbors":[5, 10, 15, 20, 25], "assign_labels":["kmeans","discretize", "cluster_qr"], "n_jobs":[-1], "random_state":[42]},
+#                                                                                            {"n_clusters":[5, 6, 7, 8, 9, 10], "n_components":[3,5,7], "affinity":["rbf", "cosine", "laplacian", "sigmoid"], "gamma":[1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1], "assign_labels":["kmeans","discretize", "cluster_qr"], "random_state":[42]},
+#                                                                                            {"n_clusters":[5, 6, 7, 8, 9, 10], "n_components":[3,5,7], "affinity":["polynomial"], "degree":[3,4,5,6,7], "assign_labels":["kmeans","discretize", "cluster_qr"], "random_state":[42]},
+#                                                                                            ]},
     
-    "guassian": {"constructor":CustomGaussianMixture, "type":"guassian", "params_dict":[{"n_components":[5, 6, 7, 8, 9, 10], "covariance_type":["full", "tied", "diag", "spherical"], "tol":[1e-6, 1e-5, 1e-4, 1e-3, 1e-2], "n_init":[1, 10, 50], "init_params":["k-means++", "random_from_data"], "reg_covar":[1e-4], "random_state":[42]},
-                                                                                  {"n_components":[5, 6, 7, 8, 9, 10], "covariance_type":["full", "tied", "diag", "spherical"], "tol":[1e-6, 1e-5, 1e-4, 1e-3, 1e-2], "warm_start":[True], "init_params":["k-means++", "random_from_data"], "reg_covar":[1e-4], "random_state":[42]},
-                                                                                  ]},
-}
+#     "guassian": {"constructor":CustomGaussianMixture, "type":"guassian", "params_dict":[{"n_components":[5, 6, 7, 8, 9, 10], "covariance_type":["full", "tied", "diag", "spherical"], "tol":[1e-6, 1e-5, 1e-4, 1e-3, 1e-2], "n_init":[1, 10, 50], "init_params":["k-means++", "random_from_data"], "reg_covar":[1e-4], "random_state":[42]},
+#                                                                                   {"n_components":[5, 6, 7, 8, 9, 10], "covariance_type":["full", "tied", "diag", "spherical"], "tol":[1e-6, 1e-5, 1e-4, 1e-3, 1e-2], "warm_start":[True], "init_params":["k-means++", "random_from_data"], "reg_covar":[1e-4], "random_state":[42]},
+#                                                                                   ]},
+# }
 
 vectorizer_config = None
 ctfidf_config = None
 representation_config = None
 
 
-topic_model = topicModel(train_docs=train_docs, test_docs=test_docs, embedding_config=embedding_config, clustering_config=clustering_config, vectorizer_config=vectorizer_config, ctfidf_config=ctfidf_config, representation_config=representation_config, save_dir=save_dir)
+# topic_model = topicModel(train_docs=train_docs, test_docs=test_docs, embedding_config=embedding_config, clustering_config=clustering_config, vectorizer_config=vectorizer_config, ctfidf_config=ctfidf_config, representation_config=representation_config, save_dir=save_dir)
+topic_model = topicModel(train_docs=train_docs, test_docs=test_docs, embedding_config=embedding_selection_config, save_dir=save_dir)
+topic_model.run_selection()
 # topic_model.run(load_embeddings=True)
 # topic_model.get_evaluation_scores(load_embeddings=True, gt_dir="data")
-topic_model.save_best_scores()
+# topic_model.save_best_scores()
