1925751f74115541c8deb4f3a58325154021ede0
krakshil <krakshil07@gmail.com>
2024-04-18 14:54:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ train_docs = pd.read_excel(os.path.join(data_dir, train_file))["Follow - up Ques
 test_docs = pd.read_excel(os.path.join(data_dir, test_file))["Follow - up Question"].values.tolist()
 
 # save_dir = "data"
-save_dir = os.path.join("data_v1","results","baseline")
+save_dir = os.path.join("data","results","baseline")
 
 # ## Add hyper-parameter combinations for embedding selection
 # embedding_selection_config = {
