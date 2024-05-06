82ef83f8981f8123ba828c2cadf6d5fa38f38f0a
krakshil <krakshil07@gmail.com>
2024-05-06 10:08:41 -0400

    Update cluster_ensemble.py

--- cluster_ensemble.py
+++ cluster_ensemble.py
@@ -283,7 +283,7 @@ class ClusterEnsemble:
                 json.dump(merge_dict, f)
         
 
-        for embedding_model in self.membership_matrices.keys()[1:]:
+        for embedding_model in list(self.membership_matrices.keys())[1:]:
             print("[INFO] Embedding Model: " + str(embedding_model) + "...")
             matrix = self.membership_matrices[embedding_model].copy()
             columns = matrix.columns
