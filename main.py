3f9dcb9bc44f7c0954c9b4fea23ad857d79de7ac
krakshil <krakshil07@gmail.com>
2024-05-26 14:09:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ representation_config = None
 
 ensemble_model = ClusterEnsemble(train_docs=train_docs, test_docs=test_docs, members_dir=save_dir_en, save_dir=os.path.join("data","results"))
 # # ensemble_model.get_partial_membership()
-ensemble_model.combine_partial_membership_matrix(ver=3, n_threshold=100, load=False)
+ensemble_model.combine_partial_membership_matrix(ver=3, n_threshold=200, load=False)
 print("\n\n")
 total_clusters = 0
 for key, matrix in ensemble_model.membership_matrices.items():
